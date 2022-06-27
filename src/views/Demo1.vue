72754fb93778741ead3b1765fbecc5fef2512130
zhan <zhan@qq.com>
2022-06-27 09:17:34 +0800

    master

---
+++ src/views/Demo1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>11111111111
+
+     <tr>
+        <th></th>
+     </tr>
+     <tr>
+        <td></td>
+     </tr>
+     
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+     return{
+        goodlist:[]
+     }
+  }
+}
+</script>
+
+<style>
+
+</style>
